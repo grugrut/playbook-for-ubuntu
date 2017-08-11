50e8de3f45b68fa99e81f081f8f5f1dcd8d30b79
grugrut <grugruglut+github@gmail.com>
2017-08-11 21:52:11 +0900

    Add nodejs setting

--- setup.sh
+++ setup.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-apt-get update
-apt-get -y install software-properties-common
-apt-add-repository -y ppa:ansible/ansible
-apt-get update
-apt-get -y install ansible
+sudo apt-get update
+sudo apt-get -y install software-properties-common
+sudo apt-add-repository -y ppa:ansible/ansible
+sudo apt-get update
+sudo apt-get -y install ansible
+
+ansible-playbook -K dev.yml
+
